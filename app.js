8e507eacf973a71c68622bfc777b26affd9c30de
Hadi AlAseel <ajafrnt.2010@gmail.com>
2020-07-19 18:12:40 +0300

    adding node_modules and updating urls

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const container=manshoursEl.parentElement;
 
 arr=getManshours();
 setTimeout(() => {
-     fetch(`https://manshours-app-server.herokuapp.com/get`)
+     fetch(`https://manshours-app-server.herok.com/get`)
     .then(response => {
         return response.json()
     })
